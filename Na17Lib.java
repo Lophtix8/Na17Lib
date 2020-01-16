8421ec8b2db5a28021acb648b7511a8bb198a5fc
Johwe23 <31901041+Johwe23@users.noreply.github.com>
2020-01-16 14:35:20 +0100

    Update Na17Lib.java

--- Na17Lib.java
+++ Na17Lib.java
@@ -1,4 +1,4 @@
-//package na17lib;
+//package eulerproblems;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,7 +6,7 @@ import java.util.List;
 public class Na17Lib {
 
     public static void main(String[] args) {
-        System.out.println(generatePrimes(100, 2));
+        System.out.println(isPrime(0));
     }
 
     public static List<Integer> generatePrimes(int upperLimit, int lowerLimit) {
@@ -18,23 +18,46 @@ public class Na17Lib {
 
         return primes;
     }
-    /*public static int[] getPrimes(int x){
-        return null;
+    
+    public static ArrayList<Integer> getFactors(int x){
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2; i <= x/2; i++) {
+            if(x % i == 0){
+                factors.add(i);
+            }
+        }
+        return factors;
     }
-
-    public  static int changeBase(int x, int fromBase, int toBase){
-        
-    }
+//    public static int[] getPrimes(int x){
+//        return null;
+//    }
+//    public  static int changeBase(int x, int fromBase, int toBase){
+//        
+//    }
 
     public static int MGN(int x, int y){
-        return 0;
+        return x * y / GCD(x, y);
     }
 
-    public static int GCD(int x, int y){
+    public static int GCD(int x, int y){  
+//        for (int i = Math.min(x, y); i >= 2 ; i--) {
+//            if(x % i == 0 && y % i == 0){
+//                return i;
+//            }
+//        }
+//        return 1;
+        
+        for (int i = 1; i < Math.min(x, y); i++) { //Snabbare metod
+            if(Math.min(x, y) % i == 0){
+                if(y % (Math.min(x, y)/i) == 0){
+                    return Math.min(x, y)/i;
+                }
+            }
+        }
+        return 1;
+    }
 
-    }*/
-
-    public static int numSum(int[] numbers){
+    /*public static int numSum(int[] numbers){
         int sum = 0;
         for (int i : numbers) {
             sum += i;
@@ -45,11 +68,11 @@ public class Na17Lib {
     public static HashMap<Integer, Integer> getDivisors(int x){
         
     }
-
-    public static boolean isPrime(int number){
-        limit = Math.pow(number, 1/2);
-    }
     */
+    public static boolean isPrime(int x){
+        return (x <= 1) ? false : (getFactors(x).isEmpty());
+    }
+    
     public static int getBiggest(int[] arr){
         int biggest = arr[0];
         for (int i : arr){
@@ -74,13 +97,13 @@ public class Na17Lib {
         return x % d == 0;
     }
 
-    /*
     public static int nFac(int n){
-
+        return (n == 0) ? 1 : n * nFac(n - 1);
     }
 
-    public static long nFac(int n){
+    public static long nFac(long n){
+        return (n == 0) ? 1 : n * nFac(n - 1);
+    }
 
-    }*/
 
 }
